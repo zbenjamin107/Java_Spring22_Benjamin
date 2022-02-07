8e5e3e1a4259ff6426d57d6311cf00c41896764a
zbenjamin107 <zachb060302@gmail.com>
2022-02-07 10:05:53 -0500

    first week submission

---
+++ week2/HelloWorld.java
@@ -0,0 +1,7 @@
+//this is going to print the statement 'hello world'
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    
+    }
+}
---
+++ week2/HelloWorld2.java
@@ -0,0 +1,15 @@
+public class HelloWorld2 {
+  
+    public static void main(String[] args){
+        for (String s: args)
+            System.out.println("Hello "+ s + " You'll be a great neard some day");
+            System.getProperty("java.class.path");
+            System.getProperty("java.home");
+            System.getProperty("java.version");
+            System.getProperty("os.arch");
+            System.getProperty("os.versoin");
+            System.getProperty("user.dir");
+            System.getProperty("user.home");
+            System.getProperty("user.name");
+    }
+}
