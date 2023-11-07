d6cd123dfddadfe6b803550372b25f8e5a27e02d
Zachary L. Benjamin <98478363+zbenjamin107@users.noreply.github.com>
2023-11-07 16:48:50 -0500

    Update Driver.java

--- MIDTERM/Driver.java
+++ MIDTERM/Driver.java
@@ -1,14 +1,12 @@
 import javax.swing.JFrame;
-//import javax.swing.JPanel;
-
-public class Driver {
-    
-    //private World world = new World(); // NOT BEING USED
-
-    
+/*
+ * zach benjamin
+ * Driver class calls for a new painter within our JFrame window
+ */
+public class Driver {    
     private JFrame window;
-    public Driver(){// called from main 
-        window = new JFrame();// new frame 
+    public Driver(){        
+        window = new JFrame();    // create new frame and customize it
         window.setSize(750,750);// set size length and width
         window.setLocationRelativeTo(null);// default location is in the center of the screen
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// close on exit
@@ -18,6 +16,6 @@ public class Driver {
     }
 
     public static void main(String[] args){
-        new Driver();// call for new driver 
+        new Driver(); 
     }
 }
