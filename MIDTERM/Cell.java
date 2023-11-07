26f504f7f933068cfe6572121ad6f7aef68494fd
Zachary L. Benjamin <98478363+zbenjamin107@users.noreply.github.com>
2023-11-07 17:06:31 -0500

    Update Cell.java

--- MIDTERM/Cell.java
+++ MIDTERM/Cell.java
@@ -1,28 +1,31 @@
+/*
+ * Cell class is used to fill our World
+ */
 import java.awt.Color;
 public class Cell{
     private states state;
     private Color color;
     public static enum states {EMPTY,TREE,BURNING};// states of a cell 
     public static Color[] colors = {Color.YELLOW, Color.GREEN, Color.RED};// colors that resemnble cell states
-    
-    public Cell(){// constructor of a cell. no  arguments sets the state and color as null 
+    // constructor of a cell. no  arguments sets the state and color as null
+    public Cell(){ 
         this.state = null;
         this.color = null;
     }
-    public Cell(states state){// given an argument the color and state are set through setState
-        setState(state);// call to set state
+    // given an argument the color and state are set through setState
+    public Cell(states state){
+        setState(state);
     }
+    //getters and setters for state and color
     public void setState(states state ){
-        this.state = state;// the instance is set a state
+        this.state = state;
         this.color = colors[state.ordinal()];// since the states are enumerated they can be used to gete the color 
     }
-    public void setState(){// no argument will be set to a tree
-        this.state = Cell.states.TREE;// set state as tree 
-        this.color = colors[state.ordinal()];// set color as green 
-    }
+    
     public states getState(){
-        return this.state;// hard to follow, i know
+        return this.state;
     }
+
     public Color getColor(){
         try{
             color = colors[this.state.ordinal()];// this.state.ordinale() returns a number 0-2 representing the state..
@@ -30,10 +33,5 @@ public class Cell{
         catch(Exception e){// in case the color has not been set, it will be null
             color = null;
         }
-        return color;// return color                 ..after number is retunred it can be used to get the color
+        return color;// return color                 
     }
-    
-    
-    
-
-}
